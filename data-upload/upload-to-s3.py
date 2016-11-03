54bc764d8aee701dd6fc204ca9258e5e2c781e09
Alexi Kostibas <alexi@kostibas.com>
2016-11-02 23:51:18 -0700

    we use json now

--- data-upload/upload-to-s3.py
+++ data-upload/upload-to-s3.py
@@ -8,7 +8,7 @@ import tempfile
 
 from datetime import datetime
 
-DATAFILE = '/mnt/sda1/data/robo-sensor.csv'
+DATAFILE = '/mnt/sda1/data/robo-sensor.json'
 DATAFILE_NAME, DATAFILE_EXT = os.path.splitext(DATAFILE)
 DATAFILE_DIR = os.path.dirname(DATAFILE)
 BUCKET = 'alexi-littlechina'
